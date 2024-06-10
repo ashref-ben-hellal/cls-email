2de458f5c52507af0cecfb44bb75675c2c6252b2
Ashref Ben hellal <achref.benhelel@gmail.com>
2024-06-11 00:08:44 +0200

    Delete .ansible directory

--- .ansible/collections/ansible_collections/cisco/intersight/playbooks/crontab_playbook.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/hayfa.ansible/collections/ansible_collections/cisco/intersight/playbooks
-ansible-playbook get_physical_summer.yml -i inventory --vault-password-file=vault.txt
-
